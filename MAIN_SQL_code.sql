b5273df7d83c33fb00ea47f256c88856f3878c02
David Ton <97371487+ton-david@users.noreply.github.com>
2022-02-19 18:26:55 -0500

    Update MAIN_SQL_code.sql

--- MAIN_SQL_code.sql
+++ MAIN_SQL_code.sql
@@ -1,11 +1,13 @@
 /*
 Marvel Cinematic Universe Data Exploration
-Skills used: CTE's, Window Functions, Aggregate Functions, Subqueries, Join, Operator, Data Cleaning, Data Scraping
+Skills used: Common Table Expressions (CTE), Window Functions, Partitions, Aggregate Functions, Subqueries, Joins, Operator, Data Cleaning
 */
+
 
 /*		Movie Queries		*/
 
-# MCU's revenue per year (SKILLS: Aggregate Function)
+# MCU's revenue per year
+	# SKILLS: Aggregate Function
 SELECT
 	EXTRACT(year FROM release_date) AS year,
     SUM(worldwide_box_office) AS annual_revenue,
@@ -13,29 +15,34 @@ SELECT
 FROM box_office
 GROUP BY year;
 
-# Revenue by movie as a percentage of total franchise revenue (SKILLS: Aggregate Window Function)
+# Revenue by movie as a percentage of total franchise revenue
+	# SKILLS: Aggregate Window Function
 SELECT movie,
-		ROUND(((worldwide_box_office/SUM(worldwide_box_office) OVER()) * 100), 2) AS percent_of_MCU_revenue
+	ROUND(((worldwide_box_office/SUM(worldwide_box_office) OVER()) * 100), 2) AS percent_of_MCU_revenue
 FROM box_office
 ORDER BY percent_of_MCU_revenue desc;
 
-# Domestic box office compared to total box office per movie (SKILLS: Operator)
-SELECT movie, ROUND((domestic_box_office/worldwide_box_office) * 100, 2) AS percent_domestic
+# Domestic box office compared to total box office per movie
+	# SKILLS: Operator
+SELECT movie,
+	ROUND((domestic_box_office/worldwide_box_office) * 100, 2) AS percent_domestic
 FROM box_office
 ORDER BY percent_domestic;
 
-# Gross profit & margin per movie (SKILLS: CTE, Join, Operator)
+# Gross profit & margin per movie
+	# SKILLS: CTE, Join, Operator
 WITH gross AS (
 	SELECT movie,
-			release_date,
+		release_date,
             production_budget,
             worldwide_box_office,worldwide_box_office - production_budget AS gross_profit
     FROM box_office)
 SELECT gross.*,
-		ROUND((gross_profit/worldwide_box_office) * 100, 2) AS gross_margin
+	ROUND((gross_profit/worldwide_box_office) * 100, 2) AS gross_margin
 FROM gross;
 
-# Highest-grossing MCU movie each year (SKILLS: CTE, Aggregate Function, Join)
+# Highest-grossing MCU movie each year
+	# SKILLS: CTE, Aggregate Function, Join
 WITH most_successful_movie AS (
 	SELECT EXTRACT(year FROM release_date) AS year,
 			MAX(worldwide_box_office) AS revenue
@@ -45,12 +52,13 @@ SELECT most_successful_movie.*, box_office.movie
 FROM most_successful_movie
 JOIN box_office ON most_successful_movie.revenue = box_office.worldwide_box_office;
 
-# Highest-rated MCU movie each year (SKILLS: CTE, Window Function)
+# Highest-rated MCU movie each year
+	# SKILLS: CTE, Ranking Window Function, Partition
 WITH rankings AS (
 	SELECT EXTRACT(year FROM release_date) AS year,
-			movie,
-            rating,
-			RANK() OVER(PARTITION BY EXTRACT(year FROM release_date) ORDER BY rating desc) AS ranking
+		movie,
+		rating,
+		RANK() OVER(PARTITION BY EXTRACT(year FROM release_date) ORDER BY rating desc) AS ranking
 	FROM box_office
 	JOIN movie_ratings USING (movie))
 SELECT year, movie, rating
@@ -62,34 +70,38 @@ ORDER BY year desc;
 /*		Actor Queries		*/
 
 # Comparing each actor's MCU box office to career box office (MCU/career)
+	# SKILLS: Operator 
 SELECT actor,
-		role,
-		number_of_movies,
+	role,
+	number_of_movies,
         mcu_worldwide_box_office,
         career_worldwide_box_office,
         ROUND((mcu_worldwide_box_office/career_worldwide_box_office) * 100, 2) AS mcu_vs_career_boxoffice
 FROM actors
 ORDER BY number_of_movies desc;
 
-# Comparing number of leading roles to number of MCU appearances for each actor appearing in > 1 movie (SKILLS: CTE, Aggregate Function, Join)
+# Comparing number of leading roles to number of MCU appearances for each actor appearing in > 1 movie
+	# SKILLS: CTE, Aggregate Function, Join
 WITH leading_roles AS (
 	SELECT actor,
-			COUNT(*) AS number_leading_roles
-    FROM leading_cast
-    GROUP BY actor)
+		COUNT(*) AS number_leading_roles
+    	FROM leading_cast
+    	GROUP BY actor)
 SELECT leading_roles.*, 
-		number_of_movies
+	number_of_movies
 FROM leading_roles
 JOIN actors USING (actor)
 ORDER BY number_leading_roles desc;
 
-# Highest-grossing actors, only including movies in which actor had a leading role (SKILLS: CTE, Aggregate Function, Join)
+# Highest-grossing actors, only including movies in which actor had a leading role
+	# SKILLS: CTE, Aggregate Function, Join
 WITH leading_roles AS (
-	SELECT actor, COUNT(*) AS count
-    FROM leading_cast
-    GROUP BY actor)
+	SELECT actor,
+		COUNT(*) AS count
+    	FROM leading_cast
+    	GROUP BY actor)
 SELECT lc.actor,
-		SUM(worldwide_box_office) AS worldwide_revenue,
+	SUM(worldwide_box_office) AS worldwide_revenue,
         lr.count AS num_movies
 FROM box_office bo
 JOIN leading_cast lc ON bo.movie = lc.movie
